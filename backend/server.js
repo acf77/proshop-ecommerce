a9a777f2067bf7fb1ec3740cb7d92dc271c8184c
acf77 <antoniocarlos.j77@gmail.com>
2022-01-18 14:20:26 -0300

    added error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-import { products } from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
-const env = dotenv.config();
+
+dotenv.config();
 connectDB();
 
 const port = process.env.PORT || 8080;
@@ -14,14 +16,11 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(
